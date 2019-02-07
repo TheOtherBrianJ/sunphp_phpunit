308829c57dca9d3ca30df99ad10105747fcff57d
Brian Johnson <bjohnson@paylease.com>
2019-02-07 12:41:45 -0700

    implement gimmeAnd and whatsThatSpell

--- src/Cheerleader.php
+++ src/Cheerleader.php
@@ -4,7 +4,16 @@ namespace SunPHP\Cheer;
 
 class Cheerleader
 {
+    private $word = '';
+
     public function gimmeAn($letter)
     {
+        $this->word .= $letter;
+        return $letter;
+    }
+
+    public function whatsThatSpell()
+    {
+        return $this->word . '!';
     }
 }
