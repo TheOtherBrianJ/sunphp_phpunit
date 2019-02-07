9124bac10805082d3a33d345a0d500b0b86d985f
Brian Johnson <bjohnson@paylease.com>
2019-02-07 11:54:49 -0700

    write a few Cheerleader tests

--- test/CheerleaderTest.php
+++ test/CheerleaderTest.php
@@ -6,5 +6,32 @@ use PHPUnit\Framework\TestCase;
 
 class CheerleaderTest extends TestCase
 {
+    public function testGimmeAnG()
+    {
+        $cheerleader = new Cheerleader();
+        $this->assertThat($cheerleader->gimmeAn('G'),
+            $this->equalTo('G'));
+    }
 
+    /**
+     * @test
+     */
+    public function gimmeAnO()
+    {
+        $cheerleader = new Cheerleader();
+        $this->assertThat($cheerleader->gimmeAn('O'),
+            $this->equalTo('O'));
+    }
+
+    /**
+     * @test
+     */
+    public function testWhatsThatSpell()
+    {
+        $cheerleader = new Cheerleader();
+        $cheerleader->gimmeAn('G');
+        $cheerleader->gimmeAn('O');
+        $this->assertThat($cheerleader->whatsThatSpell(),
+            $this->equalTo('GO!'));
+    }
 }
